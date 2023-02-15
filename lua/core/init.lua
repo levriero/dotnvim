24fe0220fd50ac474ad192a7404404ec6edfdb02
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 16:08:22 +0000

    feat: disable loading unnecesary vim built-in plugins

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,5 +1,23 @@
 local global = require("core.global")
 
+local disable_distribution_plugins = function()
+	-- Do not load zipPlugin.vim, gzip.vim and tarPlugin.vim 
+	-- (all these plugins are related to checking files inside compressed files)
+	vim.g.loaded_gzip = 1
+	vim.g.loaded_tar = 1
+	vim.g.loaded_tarPlugin = 1
+	vim.g.loaded_vimball = 1
+	vim.g.loaded_vimballPlugin = 1
+	vim.g.loaded_zip = 1
+	vim.g.loaded_zipPlugin = 1
+
+	-- Disable sql omni completion.
+	vim.g.loaded_sql_completion = 1
+
+	-- Do not load tohtml.vim
+	vim.g.loaded_2html_plugin = 1
+end
+
 local map_leader = function()
   vim.g.mapleader = " "
   vim.g.maplocalleader = " "
@@ -32,7 +50,9 @@ local set_clipboard = function()
   end
 end
 
+
 local load_core = function()
+  disable_distribution_plugins()
   map_leader()
   set_clipboard()
 
