3becaa4e6859cbb51cbb135f41fbeb7c1d29154a
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 21:18:30 +0000

    fix(lsp): properly configure installed servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,46 +12,68 @@ return {
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 
-		-- Enable these language servers.
-		-- Additional override configuration will be passed 
-		-- to the settings field in the server config.
-		local servers = {
-			lua_ls = {
-				settings = {
-					Lua = {
-						workspace = { checkThirdParty = false },
-						telemetry = { enable = false },
-					}
-				}
-			}
-		}
-
-		-- Delegate on_attach to lsp_signature
-		local on_attach = function()
-			require("lsp_signature").on_attach({
-				bind = true,
-				floating_window = true,
-				fix_pos = true,
-				hint_enable = true,
-				hi_parameter = "Search",
-				handler_opts = { border = "rounded" },
-			})
-		end
-
 		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-		-- Set up mason and ensure servers are installed and configured
-		mason.setup()
-		mason_lspconfig.setup({ ensure_installed = vim.tbl_keys(servers) })
-		mason_lspconfig.setup_handlers({
-			function(server)
-				nvim_lsp[server].setup({
-					capabilities = capabilities,
-					on_attach = on_attach,
-					settings = servers[server]
+		local opts = {
+			on_attach = function()
+				-- Delegate to lsp_signature
+				require("lsp_signature").on_attach({
+					bind = true,
+					floating_window = true,
+					fix_pos = true,
+					hint_enable = true,
+					hi_parameter = "Search",
+					handler_opts = { border = "rounded" },
 				})
+			end,
+			capabilities = capabilities,
+		}
+
+		-- Set up mason
+		mason.setup()
+
+		-- Ensure the following servers are installed
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"lua_ls"
+			}
+		})
+
+		-- Automatic server setup
+		-- see `:help mason-lspconfig-automatic-server-setup`
+		mason_lspconfig.setup_handlers({
+			function (server_name) -- default handler
+				require("lspconfig")[server_name].setup({
+					capabilities = opts.capabilities,
+					on_attach = opts.on_attach
+				})
+			end,
+
+			lua_ls = function ()
+				local _opts = {
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+								disable = { "different-requires" },
+							},
+							workspace = {
+								library = {
+									[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+									[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+								},
+							},
+							telemetry = { enable = false },
+							-- Do not override treesitter lua highlighting with lua_ls's highlighting
+							semantic = { enable = false },
+						},
+					},
+				}
+				local final_opts = vim.tbl_deep_extend("keep", _opts, opts)
+
+				nvim_lsp.lua_ls.setup(final_opts)
 			end
 		})
 	end
