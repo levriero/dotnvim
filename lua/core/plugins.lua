a32912b12aba92f6a9b1daa7425260b0e388ddba
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 23:22:27 +0000

    chore: ui settings for Lazy

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,5 +1,14 @@
 local global = require("core.global")
 local data_dir = global.data_dir
+
+local icons = {
+	kind = require("modules.utils.icons").get("kind"),
+	documents = require("modules.utils.icons").get("documents"),
+	ui = require("modules.utils.icons").get("ui"),
+	ui_sep = require("modules.utils.icons").get("ui", true),
+	misc = require("modules.utils.icons").get("misc"),
+}
+
 local Lazy = {}
 
 function Lazy:load_lazy()
@@ -12,26 +21,40 @@ function Lazy:load_lazy()
 			-- try to load one of these colorschemes when starting an installation during startup
 			colorscheme = { "catppuccin" },
 		},
+		ui = {
+			border = "rounded",
+			icons = {
+				cmd = icons.misc.Code,
+				config = icons.ui.Gear,
+				event = icons.kind.Event,
+				ft = icons.documents.Files,
+				init = icons.misc.ManUp,
+				import = icons.documents.Import,
+				keys = icons.ui.Keyboard,
+				loaded = icons.ui.Check,
+				not_loaded = icons.misc.Ghost,
+				plugin = icons.ui.Package,
+				runtime = icons.misc.Vim,
+				source = icons.kind.StaticMethod,
+				start = icons.ui.Play,
+				list = {
+					icons.ui_sep.BigCircle,
+					icons.ui_sep.BigUnfilledCircle,
+					icons.ui_sep.Square,
+					icons.ui_sep.ChevronRight,
+				}
+			}
+		},
 		performance = {
 			cache = {
 				enabled = true,
+				path = vim.fn.stdpath("cache") .. "/lazy/cache",
 			},
 			reset_packpath = true, -- reset the package path to improve startup time
 			rtp = {
 				reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
 				---@type string[]
 				paths = {}, -- add any custom paths here that you want to includes in the rtp
-				---@type string[] list any plugins you want to disable here
-				disabled_plugins = {
-					-- "gzip",
-					-- "matchit",
-					-- "matchparen",
-					-- "netrwPlugin",
-					-- "tarPlugin",
-					-- "tohtml",
-					-- "tutor",
-					-- "zipPlugin",
-				},
 			},
 		},
 	}
