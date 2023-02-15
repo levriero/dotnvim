10788077a11eb3dbe36f9d2d5cd4f4a2f46e7192
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 21:17:00 +0000

    feat: remove trailing whitespace autocommand

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,12 +1,16 @@
-local autocmd = {}
-local cmd = vim.api.nvim_command
-
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
   pattern = "*",
-  callback = function() 
+  callback = function()
     vim.highlight.on_yank()
   end
+})
+
+-- [[ Remove trailing whitespace ]]
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("RemoveTrailingWhitespace", { clear = true }),
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })
