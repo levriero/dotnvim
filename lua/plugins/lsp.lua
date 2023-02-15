b770304b2295bce4fc8805499df530bdc6b7b134
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 22:16:52 +0000

    chore: UI updates for mason and general lsp cleanup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,13 @@ return {
 		local nvim_lsp = require("lspconfig")
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
+		local icons = {
+			ui = require("modules.utils.icons").get("ui", true),
+			misc = require("modules.utils.icons").get("misc", true)
+		}
+
+		-- Set up Lsp border
+		require("lspconfig.ui.windows").default_options.border = "single"
 
 		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -19,20 +26,32 @@ return {
 		local opts = {
 			on_attach = function()
 				-- Delegate to lsp_signature
+				-- see `:h lsp_signature-full_configuration_(with_default_values)`
 				require("lsp_signature").on_attach({
 					bind = true,
 					floating_window = true,
 					fix_pos = true,
 					hint_enable = true,
-					hi_parameter = "Search",
-					handler_opts = { border = "rounded" },
+					hi_parameter = "LspSignatureActiveParameter",
+					handler_opts = {
+						border = "rounded"
+					},
 				})
 			end,
 			capabilities = capabilities,
 		}
 
 		-- Set up mason
-		mason.setup()
+		mason.setup({
+			ui = {
+				border = "rounded",
+				icons = {
+					package_pending = icons.ui.Modified_alt,
+					package_installed = icons.ui.Check,
+					package_uninstalled = icons.misc.Ghost,
+				}
+			}
+		})
 
 		-- Ensure the following servers are installed
 		mason_lspconfig.setup({
@@ -44,14 +63,14 @@ return {
 		-- Automatic server setup
 		-- see `:help mason-lspconfig-automatic-server-setup`
 		mason_lspconfig.setup_handlers({
-			function (server_name) -- default handler
+			function(server_name) -- default handler
 				require("lspconfig")[server_name].setup({
 					capabilities = opts.capabilities,
 					on_attach = opts.on_attach
 				})
 			end,
 
-			lua_ls = function ()
+			lua_ls = function()
 				local _opts = {
 					settings = {
 						Lua = {
