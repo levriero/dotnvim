b5e14468a8d145a4423a6e185c88c115b97e55ad
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 16:08:56 +0000

    fix: proper tabs

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,17 +2,22 @@ local global = require("core.global")
 
 vim.o.encoding = 'utf-8'
 vim.o.clipboard = "unnamedplus"
+
 vim.o.termguicolors = true
+vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20" -- display cursor as single line when in insert mode
+
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.autoindent = true
 
--- display cursor as single line when in insert mode
-vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+vim.o.autoindent = true
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.infercase = true
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
 vim.o.completeopt = 'menuone,noselect'
 vim.o.updatetime = 50
 
