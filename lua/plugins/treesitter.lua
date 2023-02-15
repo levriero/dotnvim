af40addc079401ebb83178b6e48f8704b701cd96
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 23:39:55 +0000

    fix: disable treesitter for vimscript and large files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -34,6 +34,20 @@ return {
 				-- `false` will disable the whole extension
 				enable = true,
 
+				disable = function(lang, buf)
+					-- Disable treesitter for vimscript
+					if vim.tbl_contains({ "vim" }, lang) then
+						return true
+					end
+
+					-- Also disable for large files
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+
 				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 				-- Using this option may slow down your editor, and you may see some duplicate highlights.
