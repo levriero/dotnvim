06dae9562c8baa0af3b7e4cd96fb5f9d74e378dd
Gus B <807237+levriero@users.noreply.github.com>
2023-02-15 22:59:53 +0000

    feat: better ui support for cmp, add cmp_luasnip source

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,6 +8,7 @@ return {
 		{ "hrsh7th/cmp-path" },
 		{ "hrsh7th/cmp-buffer" },
 		{ "onsails/lspkind.nvim" },
+		{ "saadparwaiz1/cmp_luasnip" },
 		{
 			"L3MON4D3/LuaSnip",
 			dependencies = { "rafamadriz/friendly-snippets" },
@@ -45,8 +46,36 @@ return {
 	config = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
+
+		local icons = {
+			kind = require("modules.utils.icons").get("kind"),
+			type = require("modules.utils.icons").get("type"),
+			cmp = require("modules.utils.icons").get("cmp"),
+		}
+
+		local border = function(hl)
+			return {
+				{ "╭", hl },
+				{ "─", hl },
+				{ "╮", hl },
+				{ "│", hl },
+				{ "╯", hl },
+				{ "─", hl },
+				{ "╰", hl },
+				{ "│", hl },
+			}
+		end
 
 		cmp.setup({
+			window = {
+				completion = {
+					border = border("Normal"),
+				},
+				documentation = {
+					border = border("CmpDocBorder"),
+				},
+			},
 			snippet = {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
@@ -58,6 +87,22 @@ return {
 				{ name = "luasnip" },
 				{ name = "path" },
 				{ name = "buffer" },
+			},
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					local kind = lspkind.cmp_format({
+								mode = "symbol_text",
+								maxwidth = 50,
+								symbol_map = vim.tbl_deep_extend("force", icons.kind, icons.type, icons.cmp),
+							})(entry, vim_item)
+					local strings = vim.split(kind.kind, "%s", { trimempty = true })
+
+					kind.kind = " " .. strings[1] .. " "
+					kind.menu = "    (" .. strings[2] .. ")"
+
+					return kind
+				end,
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
@@ -78,8 +123,8 @@ return {
 				["<S-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
+					elseif luasnip.jumpable( -1) then
+						luasnip.jump( -1)
 					else
 						fallback()
 					end
