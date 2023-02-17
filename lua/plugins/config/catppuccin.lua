b6b0275eeae260af0b02653978b060627a67dff0
Gus B <807237+levriero@users.noreply.github.com>
2023-02-17 22:40:56 +0000

    chore: fix theme integrations

--- lua/plugins/config/catppuccin.lua
+++ lua/plugins/config/catppuccin.lua
@@ -2,5 +2,36 @@ return function()
 	require("catppuccin").setup({
 		flavour = "mocha", -- latte, frappe, macchiato, mocha
 		transparent_background = true,
+		integrations = {
+			cmp = true,
+			fidget = true,
+			gitsigns = true,
+			indent_blankline = {
+				enabled = true,
+				colored_indent_levels = false,
+			},
+			leap = true,
+			mason = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+				},
+			},
+			notify = true,
+			nvimtree = true,
+			telescope = true,
+			treesitter = true,
+			which_key = true,
+		}
 	})
 end
