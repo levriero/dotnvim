feae50fe05d5418eb16ee6bea777bd440ed12d7e
Gus B <807237+levriero@users.noreply.github.com>
2023-02-17 23:37:15 +0000

    chore: gitsigns binding changes

--- lua/plugins/config/gitsigns.lua
+++ lua/plugins/config/gitsigns.lua
@@ -43,43 +43,43 @@ return function()
 			end
 
 			-- Navigation
-			vim.keymap.set('n', ']h', function()
+			vim.keymap.set("n", "]h", function()
 				if vim.wo.diff then
-					return ']h'
+					return "]h"
 				end
 
 				vim.schedule(function()
 					gs.next_hunk()
 				end)
 
-				return '<Ignore>'
-			end, { expr = true, desc = 'Next Hunk' })
+				return "<Ignore>"
+			end, { expr = true, desc = "Next Hunk" })
 
-			vim.keymap.set('n', '[h', function()
+			vim.keymap.set("n", "[h", function()
 				if vim.wo.diff then
-					return '[h'
+					return "[h"
 				end
 
 				vim.schedule(function()
 					gs.prev_hunk()
 				end)
 
-				return '<Ignore>'
-			end, { expr = true, desc = 'Prev Hunk' })
+				return "<Ignore>"
+			end, { expr = true, desc = "Prev Hunk" })
 
 			-- Actions
-			map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
-			map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
-			map('n', '<leader>hu', gs.undo_stage_hunk, 'Undo Stage Hunk')
-			map('n', '<leader>hp', gs.preview_hunk, 'Preview Hunk')
-			map('n', '<leader>hS', gs.stage_buffer, 'Stage buffer')
-			map('n', '<leader>hR', gs.reset_buffer, 'Reset buffer')
-			map('n', '<leader>hb', function() gs.blame_line { full = true } end, 'Blame line')
-			map('n', '<leader>hd', gs.diffthis, 'Diff this')
-			map('n', '<leader>hD', function() gs.diffthis('~') end, 'Diff this ~')
+			map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+			map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+			map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
+			map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
+			map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+			map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+			map("n", "<leader>hb", function() gs.blame_line { full = true } end, "Blame line")
+			map("n", "<leader>hd", gs.diffthis, "Diff this")
+			map("n", "<leader>hD", function() gs.diffthis("~") end, "Diff this ~")
 
 			-- Text object
-			map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
+			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 		end
 	})
 end
