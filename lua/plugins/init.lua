c32d3fa4c93011cbd94612b3abf642d5aa424e19
Gus B <807237+levriero@users.noreply.github.com>
2023-03-10 18:12:34 +0000

    chore: remove unused colorschemes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,8 +7,6 @@ table.insert(M, {
 	config = require("plugins.config.catppuccin")
 })
 
-table.insert(M, { "ellisonleao/gruvbox.nvim", })
-table.insert(M, { "folke/tokyonight.nvim" })
 table.insert(M, {
 	'rose-pine/neovim',
 	name = 'rose-pine',
