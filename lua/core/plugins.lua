72ef8f2beb7c0d016e91e88b43f17811a7e3ac13
Gus B <807237+levriero@users.noreply.github.com>
2023-07-01 23:02:13 +0100

    chore: change onload colorscheme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -19,7 +19,7 @@ function Lazy:load_lazy()
 			-- install missing plugins on startup. This doesn't increase startup time.
 			missing = true,
 			-- try to load one of these colorschemes when starting an installation during startup
-			colorscheme = { "rose-pine" },
+			colorscheme = { "tokyo-night" },
 		},
 		ui = {
 			border = "single",
