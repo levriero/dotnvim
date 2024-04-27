addf1225962a31a4b50a1a1bb5af34dd93d0c651
Gus B <807237+levriero@users.noreply.github.com>
2024-04-27 23:40:36 +0100

    mini, lualine, and completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,20 +6,16 @@ return {
 			-- Snippet Engine & its associated nvim-cmp source
 			{
 				"L3MON4D3/LuaSnip",
-				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					return "make install_jsregexp"
-				end)(),
+				version = "v2.*",
+				build = "make install_jsregexp",
 				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip").filetype_extend("ruby", { "rails" })
+							require("luasnip.loaders.from_vscode").lazy_load()
+						end,
+					},
 				},
 			},
 			"saadparwaiz1/cmp_luasnip",
