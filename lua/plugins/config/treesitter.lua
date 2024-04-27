9b44369d80bd7f15a2c08aa04632d298ae1bf503
Gus B <807237+levriero@users.noreply.github.com>
2024-04-27 17:48:33 +0100

    chore: update tree-sitter

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -21,9 +21,14 @@ return function()
 		},
 		-- Install parsers synchronously (only applied to `ensure_installed`)
 		sync_install = false,
+
 		-- Automatically install missing parsers when entering buffer
 		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 		auto_install = true,
+
+		-- List of parsers to ignore installing (or "all")
+		ignore_install = {},
+
 		highlight = {
 			-- `false` will disable the whole extension
 			enable = true,
