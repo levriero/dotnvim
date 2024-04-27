4a75108d1667c3cc9d89a95e8510b01712c466e0
Gus B <807237+levriero@users.noreply.github.com>
2024-04-27 20:35:15 +0100

    add tokyonight plugin

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		opts = {
+			style = "night",
+		},
+		init = function()
+			vim.cmd.colorscheme("tokyonight-night")
+
+			-- You can configure highlights by doing something like:
+			-- vim.cmd.hi 'Comment gui=none'
+		end,
+	},
+}
+
+-- vim: ts=2 sts=2 sw=2 et
